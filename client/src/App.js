60ceffc9a32ae07bbb02a1c759f455c69cb7c63f
Marcin <marcinegzmont@gmail.com>
2023-02-14 19:14:54 +0000

    added navbar and some icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import { ChackraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <ChackraProvider>
+    <ChakraProvider>
       <Router>
         <Navbar />
         <main></main>
       </Router>
-    </ChackraProvider>
+    </ChakraProvider>
   );
 }
 
