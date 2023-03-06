19f7a3caacf8782aa31e7099193c78141363f683
Marcin <marcinegzmont@gmail.com>
2023-03-06 21:37:15 +0000

    backed initialised

---
+++ server/database.js
@@ -0,0 +1,19 @@
+//connection to database
+import mongoose from "mongoose";
+
+const connectToDatabase = async () => {
+  console.log(process.env.MONGO_URI);
+
+  try {
+    mongoose.set("strictQuery", false);
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      //   useUnifiedToopology: true,
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected ${connect.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+export default connectToDatabase;
---
+++ server/models/Product.js
@@ -0,0 +1,60 @@
+import { Timestamp } from "bson";
+import mongoose from "mongoose";
+import { boolean } from "webidl-conversions";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    title: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  review: { reviewSchema },
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  numberOFReview: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  productIsNew: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
